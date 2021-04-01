97a112687c4236949719b3af6628611f981522f3
klarakresnik <klara.kresnik8@gmail.com>
2021-04-01 17:39:44 +0200

    Spremenila tipe

--- Gomoku/src/logika/Vrsta.java
+++ Gomoku/src/logika/Vrsta.java
@@ -1,22 +1,25 @@
 package logika;
 
-import java.util.Arrays;
-
-//Objekt, ki predstavlja vrsto petih polj na plošèi
+import java.util.LinkedList;
 
 public class Vrsta {
 	// Vrsta na plošèi je predstavljena z dvema tabelama dolžine 5.
-	// To sta tabeli x in y koordinat.bla
-	public int[] x;
-	public int[] y;
+	// To sta tabeli x in y koordinat.
+	public LinkedList<Integer> x;
+	public LinkedList<Integer> y;
+	private LinkedList<LinkedList<Integer>> holder;
 	
-	public Vrsta(int[] x, int y[]) {
+	public Vrsta(LinkedList<Integer> x, LinkedList<Integer> y) {
 		this.x = x;
 		this.y = y;
+		holder = new LinkedList<LinkedList<Integer>>();
+		holder.add(x);
+		holder.add(y);
 	}
 
 	@Override
 	public String toString() {
-		return "Vrsta [x=" + Arrays.toString(x) + ", y=" + Arrays.toString(y) + "]";
+		//return "Vrsta [x=" + LinkedList.toString(x) + ", y=" + LinkedList.toString(y) + "]";
+		return "TO DO";
 	}
 }
