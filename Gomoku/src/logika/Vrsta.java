a29639b1eac7d99982d5ffe626b112684c0caba4
klarakresnik <klara.kresnik8@gmail.com>
2021-04-02 22:09:48 +0200

    Dodala metodi equals and hashCode

--- Gomoku/src/logika/Vrsta.java
+++ Gomoku/src/logika/Vrsta.java
@@ -1,5 +1,6 @@
 package logika;
 
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Vrsta {
@@ -19,24 +20,45 @@ public class Vrsta {
 
 	@Override
 	public String toString() {
-		String niz = "[";
-		for (int i = 0; i < x.size(); ++i) {
-			niz += Integer.toString(x.get(i));
-			if (i != x.size() - 1 ) {
-				niz += "; ";
-			}
-		}
-		niz += "], [";
-		for (int i = 0; i < y.size(); ++i) {
-			niz += Integer.toString(y.get(i));
-			if (i != y.size() - 1 ) {
-				niz += "; ";
-			}
-		}
-		
-		//return "Vrsta [x=" + LinkedList.toString(x) + ", y=" + LinkedList.toString(y) + "]";
-		return niz;
+		return holder.toString();
 	}
 	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true; // hiter in pogost primer
+		if (o == null || this.getClass() != o.getClass()) return false;
+		Vrsta k = (Vrsta) o; // vemo, da je Koordinati tip objekta o
+		return this.x.equals(k.x) && this.y.equals(k.y);
+
+	}
 	
+	//menda, ce sta razlicna elementa, hashCode ne rabi bit razlicen, vazno je samo,
+	//da je za enaka elementa enak
+	@Override
+	public int hashCode() {
+		int vsota = 0;
+		for (int koordinata : x) {
+			vsota += koordinata;
+		}
+		return vsota;
+	}
+
+//preverjanje delovanje metode equals in hashCode:
+//	public static void main(String[] args){
+//		LinkedList<Integer> x1 = new LinkedList<Integer>();
+//		LinkedList<Integer> y1 = new LinkedList<Integer>();
+//		LinkedList<Integer> x2 = new LinkedList<Integer>();
+//		LinkedList<Integer> y2 = new LinkedList<Integer>();
+//		for (int i = 0; i < 10; ++i) {
+//			x1.add(i);
+//			x2.add(i);
+//		}
+//		for (int i = 10; i < 20; ++i) {
+//			y1.add(i);
+//			y2.add(i);	
+//		}
+//		Vrsta v = new Vrsta(x1, y1);
+//		Vrsta u = new Vrsta(x2, y2);
+//		System.out.println(u.equals(v));
+//	}
 }
