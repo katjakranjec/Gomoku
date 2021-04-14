dfe686fdd2c2f463bd43ee9ce34e2c4b0894c240
Katja Kranjec <Kranjec.katja1@gmail.com>
2021-04-14 12:38:59 +0200

    Posodobljeno

--- Gomoku/src/Gomoku.java
+++ Gomoku/src/Gomoku.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 public class Gomoku {
 	
 	public static void main(String[] args) throws IOException {
-
-		Vodja.igramo();
+		Vodja vodja = new Vodja();
+		vodja.igramo();
 	}
 
 }
