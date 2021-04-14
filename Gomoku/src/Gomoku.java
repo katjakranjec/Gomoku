27fde868e6886fd42d5440ac034e92dc8451aed2
Katja Kranjec <Kranjec.katja1@gmail.com>
2021-04-14 16:30:53 +0200

    Dodan grafični vmesnik

--- Gomoku/src/Gomoku.java
+++ Gomoku/src/Gomoku.java
@@ -1,11 +1,20 @@
 
 import java.io.IOException;
 
+import logika.Okno;
+
 public class Gomoku {
 	
 	public static void main(String[] args) throws IOException {
 		Vodja vodja = new Vodja();
+		//vodja.igramo();
+			
+		Okno okno = new Okno();
+		okno.pack();
+		okno.setVisible(true);
+		okno.platno.nastaviIgro(vodja.igra);
+		
 		vodja.igramo();
+		}
 	}
 
-}
