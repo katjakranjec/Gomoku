2601d9cb359a7512b383268f80bd3c5ee781b1ce
Katja Kranjec <Kranjec.katja1@gmail.com>
2021-05-27 17:37:05 +0200

    Spremenila metodo oceniVrsto - višje ocene za bolj ugodne vrste

--- Gomoku/src/inteligenca/OceniPozicijo.java
+++ Gomoku/src/inteligenca/OceniPozicijo.java
@@ -39,14 +39,34 @@ public class OceniPozicijo {
 		Polje[][] plosca = igra.getPlosca();
 		int count_W = 0;
 		int count_B = 0;
+		int zaporedni_W = 0;
+		int zaporedni_B = 0;
 		for (int k = 0; k < Igra.PET_V_VRSTO && (count_W == 0 || count_B == 0); k++) {
 			//System.out.println("V oceni vrsto se je zgubu");
 			switch (plosca[v.x[k]][v.y[k]]) {
-			case B: count_B += 1; break;
-			case W: count_W += 1; break;
-			case PRAZNO: break;
+			case B: 
+				count_B += 1; 
+				zaporedni_B += 1;
+				break;
+			case W: 
+				count_W += 1; 
+				zaporedni_W += 1;
+				break;
+			case PRAZNO: 
+				if (count_W > 0 && k != 4) zaporedni_W -= 1;
+				if (count_B > 0 && k!= 4) zaporedni_B -= 1;
+				break;
 			}
 		}
+		
+		if (count_W == 4 && jaz == Igralec.B) count_W = 1000;
+		if (count_B == 4 && jaz == Igralec.W) count_B = 1000;
+		if (count_W == 4 && jaz == Igralec.W) count_W = 25;
+		if (count_B == 4 && jaz == Igralec.B) count_B = 25;
+		if (count_W == 3 && jaz == Igralec.B && (zaporedni_W == 3 || zaporedni_W == 2)) count_W = 25;
+		if (count_B == 3 && jaz == Igralec.W && (zaporedni_B == 3 || zaporedni_B == 2)) count_B = 25;
+		
+		
 		if (count_B > 0 && count_W > 0) { return 0; }
 		else if (jaz == Igralec.B) { return count_B - count_W; }
 		else { return count_W - count_B; }
